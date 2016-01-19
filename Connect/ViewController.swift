c2dad70a04d7b48ebeef6b550396954cf8dbe293
Dylan Fiedler <dfieds913@gmail.com>
2016-01-19 14:39:11 -0600

    Initial Commit

---
+++ Connect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Connect
+//
+//  Created by Dylan Fiedler on 1/19/16.
+//  Copyright © 2016 xor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
